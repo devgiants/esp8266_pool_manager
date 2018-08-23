adf99a8ad97d0fa0f79dfed3322f51732b618b4e
Nicolas BONNIOT <nicolas@devgiants.fr>
2018-08-23 20:13:26 +0200

    Add bash to quick start project when retrieved

---
+++ init_project.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Retrieve LCD project
+git clone git@github.com:devgiants/esp8266_hd44780_i2c.git lcd
+
+# Retrieve init project 
+git clone git@github.com:devgiants/esp8266_init.git init
+
