4e62ab0e2d8a7f78ac5b2feb48a31f6f3cfa129a
Nicolas BONNIOT <nicolas@devgiants.fr>
2018-09-24 12:04:22 +0200

    Fresh start for Arduino mode

--- init_project.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Retrieve LCD project
-git clone git@github.com:devgiants/esp8266_hd44780_i2c.git lcd
-
-# Retrieve init project 
-git clone git@github.com:devgiants/esp8266_init.git init
-
